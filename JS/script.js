f6376be7a6b97f6418c96a40724c51037d25c850
nakamurasyunki <shiyunki_nakamura@mail.toyota.co.jp>
2023-08-28 17:22:08 +0900

    処理追加

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,39 @@
 function test(actual, expected) {
   if (JSON.stringify(actual) === JSON.stringify(expected)) {
-      console.log("Test PASSED.");
+    console.log("Test PASSED.");
   } else {
-      console.error("Test FAILED. Keep trying!");
-      console.group("Result:");
-      console.log(actual);
-      console.log(expected);
-      console.trace();
-      console.groupEnd();
+    console.error("Test FAILED. Keep trying!");
+    console.group("Result:");
+    console.log(actual);
+    console.log(expected);
+    console.trace();
+    console.groupEnd();
   }
 }
 
 // ランダムで飲み物を注文
-const drinkMenu = ["coffee","milk","cafe latte"];
+const drinkMenu = ["Coffee", "Milk", "Cafe Latte"];
 
 let orderDrink = drinkMenu[Math.floor(Math.random() * drinkMenu.length)];
+document.getElementById("order").innerHTML = "Hi! " + orderDrink + " ,please!";
 console.log(orderDrink)
 
 // 提供する飲み物
 function offerDrink(drink) {
-  if (drink === "coffee"){
-    return "coffee";
-  } else if (drink === "milk") {
-    return "milk";
+  if (drink === "Coffee") {
+    return "Coffee";
+  } else if (drink === "Milk") {
+    return "Milk";
   } else {
-    return "cafe latte";
+    return "Cafe Latte";
   }
 }
 
 let drink = offerDrink();
 
 // 注文された飲み物と提供した飲み物の比較
-function providedDrink(offer,order){
-  if (offer === order){
+function providedDrink(offer, order) {
+  if (offer === order) {
     return "Thank You!!";
   } else {
     return "Sorry...,You are wrong";
@@ -61,13 +62,13 @@ startModalButton.click();
 startModal()
 
 // 男女ランダム
-const guestPerson = ["man","women"];
+const guestPerson = ["man", "women"];
 
 let guestGender = guestPerson[Math.floor(Math.random() * guestPerson.length)];
 console.log(guestGender)
 
 // 性別画像判定
-const picsPerson = ["../IMG/man.jpg","../IMG/women.jpg"];
+const picsPerson = ["../IMG/man.jpg", "../IMG/women.jpg"];
 let picsNumber = 0;
 console.log(picsNumber);
 
@@ -78,31 +79,26 @@ function isGender(guestGender) {
     picsNumber = 1;
   }
   console.log(picsNumber);
-  document.getElementById("guestPerson").src=picsPerson[picsNumber];
+  document.getElementById("guestPerson").src = picsPerson[picsNumber];
 }
 isGender(guestGender);
 
-function fiveOrder(times) {
-  for (const time of times) {
-    if (time === 1) {
-      drink = offerDrink("coffee");
-    } else if (time === 2) {
-      drink = offerDrink("milk");
-    } else if (time === 3) {
-      drink = offerDrink("coffee");
-    } else if (time === 4) {
-      drink = offerDrink("cafe latte");
-    } else if (time === 5) {
-      drink = offerDrink("coffee");
-    }
-    console.log(drink);
-    result = providedDrink(drink,orderDrink);
-    console.log(result);
-    isGender(guestGender);
-    console.log(time);
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 1) {
+    drink = offerDrink("Coffee");
+  } else if (i === 2) {
+    drink = offerDrink("Milk");
+  } else if (i === 3) {
+    drink = offerDrink("Coffee");
+  } else if (i === 4) {
+    drink = offerDrink("Cafe Latte");
+  } else if (i === 5) {
+    drink = offerDrink("Coffee");
   }
+  console.log(drink);
+  result = providedDrink(drink, orderDrink);
+  console.log(result);
+  isGender(guestGender);
+  console.log(i);
 }
-
-const times = [1, 2, 3, 4, 5];
-
-console.log(fiveOrder(times));
