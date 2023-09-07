7b2979ebfa1843874dc3a1d8dc41d724d6906bce
nakamurasyunki <shiyunki_nakamura@mail.toyota.co.jp>
2023-09-07 10:15:25 +0900

    仕上げ

--- JS/music.js
+++ JS/music.js
@@ -6,7 +6,7 @@ const audio = document.getElementsByTagName("audio")[0];
 
 if (page !== null) {
   page.addEventListener("click", function () {
-    audio.volume = 0.01;
+    audio.volume = 0.03;
     audio.loop = true;
     audio.play();
   })
